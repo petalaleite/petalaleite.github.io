b2906aa81e568d55473459cc9b2d6d9a1b1bc59e
Pétala Leite <petalaleite@gmail.com>
2024-04-02 14:06:09 -0300

    fix: center navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
   return (
-    <Navbar bg="primary">
-      <Container>
-        <Nav className='justify-content-center'>
-          <Nav.Item>
-            <Nav.Link href="#home">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Container>
+    <Navbar bg="primary" className="justify-content-center">
+      <Nav>
+        <Nav.Item>
+          <Nav.Link href="#home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </Navbar>
   );
 }
