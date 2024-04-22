d403720b88f174dd69b7c07562fe1a09ec0f471b
Pétala Leite <petalaleite@gmail.com>
2024-04-22 14:12:44 -0300

    add working email form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import { Button, Form, Image } from 'react-bootstrap';
 import emailjs from '@emailjs/browser';
-import './form.css';
 
 function ContactForm() {
 
@@ -9,25 +8,26 @@ function ContactForm() {
 
   const sendEmail = (e) => {
     e.preventDefault();
+    console.log(process.env.REACT_APP_SERVICE_ID,);
     emailjs
-      .sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form.current, { publicKey: process.env.API_PUBLIC_KEY })
+      .sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, { publicKey: process.env.REACT_APP_API_PUBLIC_KEY })
       .then(() => { console.log('SUCCESS'); }, (error) => console.log('FAILED', error.text));
   };
   return (
     <>
       <h1>Get in touch</h1>
-      <Form className='form-container rounded mx-auto d-block' ref={form}>
+      <Form className='form-container rounded mx-auto d-block' ref={form} onSubmit={sendEmail}>
         <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
-          <Form.Control />
+          <Form.Control name='user_name' />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Email</Form.Label>
-          <Form.Control type='email' placeholder='name@example.com' />
+          <Form.Control type='email' placeholder='name@example.com' name='user_email' />
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Your message</Form.Label>
-          <Form.Control as='textarea' rows={4} placeholder='Message...' />
+          <Form.Control as='textarea' rows={4} placeholder='Message...' name='message' />
         </Form.Group>
         <Button className='button' type='submit'>
           <Image src='https://img.icons8.com/material-outlined/24/filled-sent.png' />
